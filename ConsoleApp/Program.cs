1573c6d9607169261da24dd48dc99952d0de722f
pohoreckim <mikolaj.pohorecki@gmail.com>
2023-05-31 22:49:53 +0200

    Avg time measurement

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,7 +7,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Program rozegra g gier Van der Waerdena");
+        Console.WriteLine("Ile gier Van der Waerdena ma rozegrać program");
         int gamesCount = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Podaj długość zbioru dostępnych liczb");
         int numbersCount = Convert.ToInt32(Console.ReadLine());
@@ -20,11 +20,14 @@ internal class Program
 
         for (int i = 0; i < gamesCount; i++)
         {
+            Dictionary<string, List<TimeSpan>> times = new Dictionary<string, List<TimeSpan>>();
+            times.Add("pierwszy", new List<TimeSpan>());
+            times.Add("drugi", new List<TimeSpan>());
+
             Game game = new(numbersCount, winningSequenceCount);
 
-            IAlgorithm playerOne = new Uct(1.414, new IterationStopCondition(1000), game);
-            IAlgorithm playerTwo = new RandomPick(game);
-            //IAlgorithm playerTwo = new MiniMax(new IterationStopCondition(1000), game);
+            IAlgorithm playerOne = InitiatePlayerAlgorithm(firstAlg, game);
+            IAlgorithm playerTwo = InitiatePlayerAlgorithm(secondAlg, game);
 
             State currentState = game.InitialState();
             GameTree gameTree = new(currentState, game);
@@ -35,9 +38,12 @@ internal class Program
                 IAlgorithm currentPlayer = currentState.CurrentPlayer == Player.One ? playerOne : playerTwo;
                 string player = currentState.CurrentPlayer == Player.One ? "pierwszy" : "drugi";
 
+                Stopwatch stopwatch = Stopwatch.StartNew();
                 int nextAction = (int)currentPlayer.ReturnNextMove(gameTree.SelectedNode)!;
+                stopwatch.Stop();
+                times[player].Add(stopwatch.Elapsed);
 
-                Console.WriteLine($"Gracz {player} koloruje {nextAction}");
+                Console.WriteLine($"Gracz {player} koloruje {nextAction}, czas namysłu {stopwatch.Elapsed}");
                 gameTree.MoveGameToNextState(nextAction);
 
                 currentState = gameTree.SelectedNode.CorespondingState;
@@ -51,9 +57,28 @@ internal class Program
                 result = "Gracz drugi wygrywa!";
             else
                 result = "Remis";
+            Console.WriteLine(result);
+            var avg = Average(times["pierwszy"]);
+            Console.WriteLine($"Średni czas ruchu pierwszego gracza to: {avg}");
+            avg = Average(times["drugi"]);
+            Console.WriteLine($"Średni czas ruchu drugiego gracza to: {avg}");
+            Console.WriteLine("######################");
         }
+
+
     }
-    private IAlgorithm InitiatePlayerAlgorithm(string alg, Game game)
+    public static TimeSpan Average(IEnumerable<TimeSpan> timeSpans)
+    {
+        IEnumerable<long> ticksPerTimeSpan = timeSpans.Select(t => t.Ticks);
+        double averageTicks = ticksPerTimeSpan.Average();
+        long averageTicksLong = Convert.ToInt64(averageTicks);
+
+        TimeSpan averageTimeSpan = TimeSpan.FromTicks(averageTicksLong);
+
+        return averageTimeSpan;
+    }
+
+    private static IAlgorithm InitiatePlayerAlgorithm(string alg, Game game)
     {
         switch (alg)
         {
