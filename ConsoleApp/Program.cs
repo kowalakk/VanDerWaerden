57c7c93993701fe12fe86f26ea499d827fc6fa4f
pohoreckim <mikolaj.pohorecki@gmail.com>
2023-05-31 22:28:07 +0200

    Multiple games console script

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Ai;
+using System.Collections.Generic;
 using System.Diagnostics;
 using VanDerWaerden;
 
@@ -6,19 +7,25 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        while (true)
-        {
+        Console.WriteLine("Program rozegra g gier Van der Waerdena");
+        int gamesCount = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Podaj długość zbioru dostępnych liczb");
+        int numbersCount = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Podaj długość wygrywającego ciągu arytmetycznego");
+        int winningSequenceCount = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Jakim algorytmem ma grać pierwszy gracz?[UCT/MinMax/Random]");
+        string firstAlg = Console.ReadLine()!;
+        Console.WriteLine("Jakim algorytmem ma grać drugi gracz?[UCT/MinMax/Random]");
+        string secondAlg = Console.ReadLine()!;
 
-            Console.WriteLine("Program rozegra grę Van der Waerdena");
-            Console.WriteLine("Podaj długość zbioru dostępnych liczb");
-            int numbersCount = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Podaj długość wygrywającego ciągu arytmetycznego");
-            int winningSequenceCount = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < gamesCount; i++)
+        {
             Game game = new(numbersCount, winningSequenceCount);
+
             IAlgorithm playerOne = new Uct(1.414, new IterationStopCondition(1000), game);
             IAlgorithm playerTwo = new RandomPick(game);
             //IAlgorithm playerTwo = new MiniMax(new IterationStopCondition(1000), game);
- 
+
             State currentState = game.InitialState();
             GameTree gameTree = new(currentState, game);
 
@@ -31,17 +38,35 @@ internal class Program
                 int nextAction = (int)currentPlayer.ReturnNextMove(gameTree.SelectedNode)!;
 
                 Console.WriteLine($"Gracz {player} koloruje {nextAction}");
-                gameTree.SelectChildNode(nextAction);
+                gameTree.MoveGameToNextState(nextAction);
 
                 currentState = gameTree.SelectedNode.CorespondingState;
                 gameResult = game.Result(currentState);
             }
+
+            string result = "";
             if (gameResult == GameResult.PlayerOneWins)
-                Console.WriteLine($"Gracz pierwszy wygrywa!");
+                result = "Gracz pierwszy wygrywa!";
             else if (gameResult == GameResult.PlayerTwoWins)
-                Console.WriteLine($"Gracz drugi wygrywa!");
+                result = "Gracz drugi wygrywa!";
             else
-                Console.WriteLine($"Remis");
+                result = "Remis";
         }
     }
+    private IAlgorithm InitiatePlayerAlgorithm(string alg, Game game)
+    {
+        switch (alg)
+        {
+            case "UCT":
+                return new Uct(1.414, new IterationStopCondition(1000), game);
+            case "MinMax":
+                return new MiniMax(new IterationStopCondition(1000), game);
+            case "Random":
+                return new RandomPick(game);
+            default:
+                break;
+        }
+        return null;
+    }
+
 }
